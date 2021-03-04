38c508733347ed0ce8b20f5b455f1f7308614874
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-03-04 17:57:12 +0100

    Simplify SharedDependencyStatus
    We don't compare with the system for deps yet.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/action/dependency_shared.go
+++ pkg/action/dependency_shared.go
@@ -60,86 +60,8 @@ func (d *SharedDependency) List(chartpath string, out io.Writer) error {
 
 // dependecyStatus returns a string describing the status of a dependency viz a viz the parent chart.
 func (d *SharedDependency) SharedDependencyStatus(chartpath string, dep *chart.Dependency, parent *chart.Chart) string {
-	filename := fmt.Sprintf("%s-%s.tgz", dep.Name, "*")
-
-	// If a chart is unpacked, this will check the unpacked chart's `charts/` directory for tarballs.
-	// Technically, this is COMPLETELY unnecessary, and should be removed in Helm 4. It is here
-	// to preserved backward compatibility. In Helm 2/3, there is a "difference" between
-	// the tgz version (which outputs "ok" if it unpacks) and the loaded version (which outouts
-	// "unpacked"). Early in Helm 2's history, this would have made a difference. But it no
-	// longer does. However, since this code shipped with Helm 3, the output must remain stable
-	// until Helm 4.
-	switch archives, err := filepath.Glob(filepath.Join(chartpath, "charts", filename)); {
-	case err != nil:
-		return "bad pattern"
-	case len(archives) > 1:
-		// See if the second part is a SemVer
-		found := []string{}
-		for _, arc := range archives {
-			// we need to trip the prefix dirs and the extension off.
-			filename = strings.TrimSuffix(filepath.Base(arc), ".tgz")
-			maybeVersion := strings.TrimPrefix(filename, fmt.Sprintf("%s-", dep.Name))
-
-			if _, err := semver.StrictNewVersion(maybeVersion); err == nil {
-				// If the version parsed without an error, it is possibly a valid
-				// version.
-				found = append(found, arc)
-			}
-		}
-
-		if l := len(found); l == 1 {
-			// If we get here, we do the same thing as in len(archives) == 1.
-			if r := statArchiveForStatus(found[0], dep); r != "" {
-				return r
-			}
-
-			// Fall through and look for directories
-		} else if l > 1 {
-			return "too many matches"
-		}
-
-		// The sanest thing to do here is to fall through and see if we have any directory
-		// matches.
-
-	case len(archives) == 1:
-		archive := archives[0]
-		if r := statArchiveForStatus(archive, dep); r != "" {
-			return r
-		}
-
-	}
-	// End unnecessary code.
-
-	var depChart *chart.Chart
-	for _, item := range parent.Dependencies() {
-		if item.Name() == dep.Name {
-			depChart = item
-		}
-	}
-
-	if depChart == nil {
-		return "missing"
-	}
-
-	if depChart.Metadata.Version != dep.Version {
-		constraint, err := semver.NewConstraint(dep.Version)
-		if err != nil {
-			return "invalid version"
-		}
-
-		v, err := semver.NewVersion(depChart.Metadata.Version)
-		if err != nil {
-			return "invalid version"
-		}
-
-		if !constraint.Check(v) {
-			return "wrong version"
-		}
-	}
-
-	return "unpacked"
-}
-
+	// TODO try to find the shared dep, and see if it resolves: installable, not found, installed, etc.
+	return "listed"
 }
 
 // printSharedDependencies prints all of the shared dependencies in the yaml file.
